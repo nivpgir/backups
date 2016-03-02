82e87d74bc5ffb8e9f4d4f3baed8b0792db6d14e
Niv Pagir <nivpgir@gmail.com>
2016-03-02 23:52:12 +0200

    emacs improved

---
+++ custom-setups/my-projectile-setup.el
@@ -0,0 +1,14 @@
+(provide 'my-projectile-setup)
+
+;;enable projectile
+(projectile-global-mode)
+
+;;enable caching
+(setq projectile-enable-caching t)
+
+
+
+
+
+(provide 'my-projectile-setup)
+;;; my-projectile-setup.el ends here

4bcde706b05d363442e1d32dc641d3905d156ef2
Niv Pagir <nivpgir@Lochness.hustaff.huji.local>
2017-11-29 20:09:01 +0200

    removed junk from organization

--- shortcuts.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHORTCUTS
-#define SHORTCUTS
-
-#define ERR_CHECK(test, msg) 		if( (test) ){ perror(msg); exit(1); }
-
-#define handle_error_en(en, msg)	do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
-#define handle_error(msg)			do { perror(msg); exit(EXIT_FAILURE); } while (0)
-
-#endif

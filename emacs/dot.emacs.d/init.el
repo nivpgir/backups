5336125f762d8f48c86b163a699b12486691f202
Niv Pagir <niv.pagir@orcam.com>
2019-04-17 17:04:30 +0300

    emacs conf upgrades
    added bindings to find init.el and to scroll without
    moving cursor

--- emacs/dot.emacs.d/init.el
+++ emacs/dot.emacs.d/init.el
@@ -75,11 +75,22 @@ current window."
          (other-buffer current-buffer t)))))
 (define-key 'my-keymap (kbd "<tab>") 'alternate-buffer)
 
+;; indent whole buffer
 (defun indent-buffer ()
   (interactive)
   (save-excursion
     (indent-region (point-min) (point-max) nil)))
 (define-key (current-global-map) (kbd "M-<tab>") 'indent-buffer)
+
+;; go to init.el
+(defun find-user-init-file ()
+  "Edit the `user-init-file', in another window."
+  (interactive)
+  (find-file user-init-file))
+(define-key 'my-keymap (kbd "I") 'find-user-init-file)
+(define-key (current-global-map) (kbd "M-n") (lambda () (interactive) (scroll-up 1)))
+(define-key (current-global-map) (kbd "M-p") (lambda () (interactive) (scroll-down 1)))
+
 
 ;;unbinding C-m from RET
 ;; (define-key input-decode-map [?\C-m] [C-m]) ;; without this we can't RET doesn't work in terminal
